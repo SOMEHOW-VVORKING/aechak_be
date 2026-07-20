87ace7c26a3237d86673433ceaf23e04e55b9346
jher235 <tim668666@gmail.com>
2026-07-21 02:32:04 +0900

    [SCRUM-135] fix: 파일 에러코드를 110000대로 이동
    100000대가 settlement 잠정 대역과 충돌해 클라이언트가 errorCode로 두 도메인을 구분할 수 없음.

--- application/src/main/kotlin/com/aechak/application/file/error/FileErrorCode.kt
+++ application/src/main/kotlin/com/aechak/application/file/error/FileErrorCode.kt
@@ -7,7 +7,7 @@ enum class FileErrorCode(
     override val message: String,
     override val status: Int,
 ) : ErrorCode {
-    UNSUPPORTED_FILE_TYPE(100001, "지원하지 않는 파일 형식입니다.", 400),
-    FILE_NOT_FOUND(100002, "업로드된 파일을 찾을 수 없습니다.", 404),
-    FILE_ACCESS_DENIED(100003, "본인이 업로드한 파일만 사용할 수 있습니다.", 403),
+    UNSUPPORTED_FILE_TYPE(110001, "지원하지 않는 파일 형식입니다.", 400),
+    FILE_NOT_FOUND(110002, "업로드된 파일을 찾을 수 없습니다.", 404),
+    FILE_ACCESS_DENIED(110003, "본인이 업로드한 파일만 사용할 수 있습니다.", 403),
 }
