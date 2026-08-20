ccabe1ea773604f35f732616ca2c7b5655e065ec
jher235 <tim668666@gmail.com>
2026-08-20 23:30:34 +0900

    [SCRUM-205] feat: 결제 게이트웨이 포트와 연동 실패 에러코드 추가
    - 게이트웨이 실패는 원인이 무엇이든 호출부가 할 일이 결제 중단으로 같아 PAYMENT_GATEWAY_ERROR 하나로 접음
    - 이미 결제된 건만 FE가 완료 화면으로 분기해야 해서 PAYMENT_ALREADY_PAID로 가름
    - 60001부터 60004는 구 결제 APISpec이, 60010부터 60018은 정산 APISpec이 점유 중이라 60005와 60006을 씀

--- domain/src/main/kotlin/com/aechak/domain/payment/error/PaymentErrorCode.kt
+++ domain/src/main/kotlin/com/aechak/domain/payment/error/PaymentErrorCode.kt
@@ -8,4 +8,6 @@ enum class PaymentErrorCode(
     override val status: Int,
 ) : ErrorCode {
     PAYMENT_NOT_FOUND(60000, "결제 내역을 찾을 수 없습니다.", 404),
+    PAYMENT_GATEWAY_ERROR(60005, "결제 게이트웨이 호출에 실패했습니다.", 502),
+    PAYMENT_ALREADY_PAID(60006, "이미 결제가 완료된 건입니다.", 409),
 }
